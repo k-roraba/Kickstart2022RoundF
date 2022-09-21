25be6c7e1df449214a3021d9f7de7ecf7ebbc8a9
K. Rorabaugh <102931841+k-roraba@users.noreply.github.com>
2022-09-21 14:09:07 -0500

    Update KickstartRoundF2022Problem1.py

--- KickstartRoundF2022Problem1.py
+++ KickstartRoundF2022Problem1.py
@@ -1,4 +1,8 @@
-#CodeJam 2022 Round F Problem 1 - Sort the Fabric
+# CodeJam 2022 Round F Problem 1 - Sort the Fabric
+# https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb409/0000000000beefbb
+# passed the first round of tests, but got TLE (time limit error) for test set 2
+# I think maybe I have to sort as they come in, so that I don't have to sort the whole list twice.  
+# But I could be missing something bigger.
 
 cases = int(input())
 output = []
@@ -21,11 +25,12 @@ for x in range(cases):
         newFab = fabric(c, int(d), int(u))
         unsorted.append(newFab)
 
-    #sort by Ada (bubblesort)
+    #sort by Ada (bubblesort on color, c)
     for i in range(n - 1):
         for j in range(0, n - i - 1):
             if unsorted[j].c > unsorted[j+1].c:
                 unsorted[j], unsorted[j+1] = unsorted[j+1], unsorted[j]
+            # tie break condition
             if unsorted[j].c == unsorted[j+1].c:
                 if unsorted[j].u > unsorted[j+1].u:
                     unsorted[j], unsorted[j+1] = unsorted[j+1], unsorted[j]
@@ -34,17 +39,19 @@ for x in range(cases):
     for i in range(n):
         unsorted[i].AdaPos = i
     
-    #sort by Charles (bubblesort)
+    #sort by Charles (bubblesort on durability, d)
     for i in range(n - 1):
         for j in range(0, n - j - 1):
             if unsorted[j].d > unsorted[j+1].d:
                 unsorted[j], unsorted[j+1] = unsorted[j+1], unsorted[j]
+            # tie break condition
             if unsorted[j].d == unsorted[j+1].d:
                 if unsorted[j].u > unsorted[j+1].u:
                     unsorted[j], unsorted[j+1] = unsorted[j+1], unsorted[j]
     
     count = 0
     # count matches
+    # I need to rethink how bubblesort works to see if I can count during sorting
     for i in range(n):
         if unsorted[i].AdaPos == i:
             count += 1
